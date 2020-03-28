40a31639b7d0085bf3ef085db77ea149c06e025a
pavlushk0 <pavlushk0@mail.ru>
2020-03-29 00:11:04 +0400

    Win Makefile add

--- algebra2.h
+++ algebra2.h
@@ -8,6 +8,8 @@ using namespace std;
 enum {_XC, _YC, _ZC, _WC};
 enum mtrx_type {MTRX_IDTT, MTRX_ZERO};
 
+#define M_PI 3.14159265358979323846
+
 constexpr float f_eps = 5.96e-08;
 constexpr float deg_to_rad(float deg) {
 	return deg * M_PI/180.0f;
---
+++ algebra2.mk
@@ -0,0 +1,38 @@
+
+CC       := g++
+CFLAGS   := -Wall -std=c++17
+LIBS 	 := -lstdc++
+RM       := rm -f
+
+SRCS := mtrx2.o mtrx3.o mtrx4.o vec2.o vec3.o vec4.o qtnn.cpp plane.cpp
+OBJS := $(SRCS:.cpp=.o)
+
+$(info COMMON MAKEFILE)
+
+all: compile
+
+compile: mtrx2.o mtrx3.o mtrx4.o vec2.o vec3.o vec4.o qtnn.o plane.o
+
+mtrx2.o: mtrx2.cpp
+	$(CC) $(CFLAGS) -c mtrx2.cpp
+
+mtrx3.o: mtrx3.cpp
+	$(CC) $(CFLAGS) -c mtrx3.cpp 
+
+mtrx4.o: mtrx4.cpp
+	$(CC) $(CFLAGS) -c mtrx4.cpp 
+
+vec2.o: vec2.cpp
+	$(CC) $(CFLAGS) -c vec2.cpp 
+
+vec3.o: vec3.cpp
+	$(CC) $(CFLAGS) -c vec3.cpp
+
+vec4.o: vec4.cpp
+	$(CC) $(CFLAGS) -c vec4.cpp
+
+qtnn.o: qtnn.cpp
+	$(CC) $(CFLAGS) -c qtnn.cpp 
+
+plane.o: plane.cpp
+	$(CC) $(CFLAGS) -c plane.cpp
